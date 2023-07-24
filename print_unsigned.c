859a9040fa6ff3603c2d33a5737f466005a9835b
freelung22/printf.git <freebel333@gmail.com>
2023-07-24 05:59:32 -0700

    These are exercise for printf project

--- print_unsigned.c
+++ print_unsigned.c
@@ -1,19 +1,44 @@
 #include "main.h"
-
 /**
- * print_unsigned - Print a unsigned int
- * @list: Number to print
- *
- * Return: Length of the number
- **/
-int print_unsigned(va_list list)
+ * printf_unsigned - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_unsigned(va_list args)
 {
-	char *p_buff;
-	int size;
+	unsigned int n = va_arg(args, unsigned int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
 
-	p_buff = itoa(va_arg(list, unsigned int), 10);
+	n = n / 10;
+	num = n;
 
-	size = print((p_buff != NULL) ? p_buff : "NULL");
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
 
-	return (size);
+	return (i);
 }
